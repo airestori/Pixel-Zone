63f2def856b648670fd0547490b6f0a7c7e32f4e
airestori <141364880+airestori@users.noreply.github.com>
2023-12-16 23:33:32 -0300

    Delete pixel-zone directory

--- pixel-zone/js/headerStyle.js
+++
@@ -1,8 +0,0 @@
-const header = document.getElementById("header")
-
-window.addEventListener('scroll', function () {
-    var scrollPosition = window.scrollY;
-    var opacity = 0.3 + (scrollPosition / 1000);
-
-    document.getElementById('heroPage').style.background = `rgba(0, 0, 0, ${opacity})`;
-});
